9aee32be86ca636f403ad0e1d977b3da5c3ba6ee
Gabriel Garrido <gabrielgarrido89@gmail.com>
2024-06-11 23:18:53 -0400

    pagination in blog app

--- apps/blog/views.py
+++ apps/blog/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from rest_framework import permissions
+from .models import Post, ViewCount
+from apps.category.models import Category
+from .serializers import PostSerializer, PostListSerializer
+from .pagination import SmallSetPagination, MediumSetPagination, LargeSetPagination
 
-# Create your views here.
+class BlogListView(APIView):
+    permission_classes = (permissions.AllowAny, )
+    def get(self, request, format=None):
+        if Post.objects.all().exists():
+            posts = Post.objects.all()
+            
+            paginator = SmallSetPagination()
+            results = paginator.paginate_queryset(posts,request)
+            serializer = PostListSerializer(results, many=True)
+            
+            return paginator.get_paginated_response({'posts': serializer.data})
+        else:
+            return Response({'error': 'No post found'}, status=status.HTTP_404_NOT_FOUND)
+
--- core/settings.py
+++ core/settings.py
@@ -60,14 +60,19 @@ INSTALLED_APPS = DJANGO_APPS + PROJECT_APPS + THIRD_PARTY_APPS
 
 #CKEditor
 CKEDITOR_CONFIGS = {
+    # 'default': {
+    #     'toolbar': 'Custom',
+    #     'toolbar_Custom': [
+    #         ['Bold', 'Italic', 'Underline'],
+    #         ['NumberedList', 'BulletedList', '-', 'Outdent', 'Indent', '-', 'JustifyLeft', 'JustifyCenter', 'JustifyRight', 'JustifyBlock'],
+    #         ['Link', 'Unlink'],
+    #         ['RemoveFormat', 'Source']
+    #     ],
+    #     'autoParagraph': False
+    # }
+    
     'default': {
-        'toolbar': 'Custom',
-        'toolbar_Custom': [
-            ['Bold', 'Italic', 'Underline'],
-            ['NumberedList', 'BulletedList', '-', 'Outdent', 'Indent', '-', 'JustifyLeft', 'JustifyCenter', 'JustifyRight', 'JustifyBlock'],
-            ['Link', 'Unlink'],
-            ['RemoveFormat', 'Source']
-        ],
+        'toolbar': 'full',
         'autoParagraph': False
     }
 }
